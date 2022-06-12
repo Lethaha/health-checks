b833ef2c9f8da1689821c5fa779d8bcfd4dfe1fa
Lethaha <lethha.thamae@gmail.com>
2022-06-12 10:32:22 +0200

    merge old disk usage and all checks

--- all_checks.py
+++ all_checks.py
@@ -1,16 +1,34 @@
-#!/usr/bin/env python 3
+#!/usr/bin/env python3
+
 
 import os
+import shutil
 import sys
 
 def check_reboot():
 	""" Return True if the computer has a pendng reboot file """
 	return os.path.exists("/run/reboot-required")
-	
+
+
+def check_disk_usage(disk, min_absolute, min_percent):
+
+        du = shutil.disk_usage(disk)
+        # Calculate percentage of free space
+        percent_free = 100 * du.free / du.total
+
+        gigabytes_free = du.free * 2**30
+        if percent_free < min_percent or  gigabytes_free < min_absolute:
+                return False
+        return True
 def main():
 	if check_reboot():
 		print("Pending Reboot")
 		sys.exit(1)
+	
+	if not check_disk_usage("/",2*2**30, 10):
+        	print("ERROR: Not enough disk space.")
+        	sys.exit(1)
+
 	print("Everything is ok.")
 	sys.exit(0)
 main()
